9319142b2b155a8b19e66c2fac13ae001463ccb6
MadalinBorodi <49339271+MadalinBorodi@users.noreply.github.com>
2019-04-07 18:33:49 +0300

    Create elseif_example.sh

---
+++ elseif_example.sh
@@ -0,0 +1,21 @@
+#The use of else if condition is little different in bash than other programming language. ‘elif’ is used to define else if condition in bash
+
+
+#!/bin/bash
+
+echo "Enter your lucky number"
+read n
+
+if [ $n -eq 101 ];
+then
+echo "You got 1st prize"
+elif [ $n -eq 510 ];
+then
+echo "You got 2nd prize"
+elif [ $n -eq 999 ];
+then
+echo "You got 3rd prize"
+
+else
+echo "Sorry, try for the next time"
+fi
