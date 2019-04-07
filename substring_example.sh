5ba8881df84100ee30a282c16d8edb97f3297000
MadalinBorodi <49339271+MadalinBorodi@users.noreply.github.com>
2019-04-07 18:38:08 +0300

    Create substring_example.sh

---
+++ substring_example.sh
@@ -0,0 +1,7 @@
+# Here, the value, 6 indicates the starting point from where the substring will start and 5 indicates the length of the substring
+
+
+#!/bin/bash
+Str="Learn Linux from LinuxHint"
+subStr=${Str:6:5}
+echo $subStr
