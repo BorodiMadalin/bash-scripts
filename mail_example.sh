1c83bf1a24710b4323c29e80ac72fd18014e4aa5
MadalinBorodi <49339271+MadalinBorodi@users.noreply.github.com>
2019-04-07 18:48:35 +0300

    Create mail_example.sh

---
+++ mail_example.sh
@@ -0,0 +1,8 @@
+# You can send email by using ‘mail’ or ‘sendmail’ command. Before using these commands, you have to install all necessary packages
+
+
+#!/bin/bash
+Recipient=”admin@example.com”
+Subject=”Greeting”
+Message=”Welcome to our site”
+`mail -s $Subject $Recipient <<< $Message`
