df23c314defea90e765e9b5badbc745e6a72ecc9
MadalinBorodi <49339271+MadalinBorodi@users.noreply.github.com>
2019-04-07 18:34:43 +0300

    Create case_example.sh

---
+++ case_example.sh
@@ -0,0 +1,17 @@
+#Case statement is used as the alternative of if-elseif-else statement. The starting and ending block of this statement is defined by ‘case’ and ‘esac’
+
+
+#!/bin/bash
+
+echo "Enter your lucky number"
+read n
+case $n in
+101)
+echo echo "You got 1st prize" ;;
+510)
+echo "You got 2nd prize" ;;
+999)
+echo "You got 3rd prize" ;;
+*)
+echo "Sorry, try for the next time" ;;
+esac
