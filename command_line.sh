8e46874354fc7784b516c7fb21d684a41eed7d2a
MadalinBorodi <49339271+MadalinBorodi@users.noreply.github.com>
2019-04-07 18:35:38 +0300

    Create command_line.sh

---
+++ command_line.sh
@@ -0,0 +1,6 @@
+# Bash script can read input from command line argument like other programming language. For example, $1 and $2 variable are used to read first and second command line arguments
+
+#!/bin/bash
+echo "Total arguments : $#"
+echo "1st Argument = $1"
+echo "2nd argument = $2"
